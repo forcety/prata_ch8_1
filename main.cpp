5c283436740c0b772dc1cf69f1c07917a9329df2
forcety <forcety@mail.ru>
2018-05-01 16:19:28 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int globalCounter = 0;
+
+void ShowStr(const std::string& str)
+{
+   globalCounter++;
+   std::cout << str << "\n";
+}
+
+void ShowStr(const std::string& str, const int val)
+{
+    globalCounter++;
+
+    if (val != 0) {
+        for (int i = 0; i < globalCounter; ++i) {
+          std::cout << str << "\n";
+        }
+
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Start program.\n";
+
+    QCoreApplication a(argc, argv);
+
+    ShowStr("First");
+    ShowStr("Second", 1);
+    ShowStr("Third", 1);
+
+    std::cout << "Finish program.\n";
+    return a.exec();
+}
